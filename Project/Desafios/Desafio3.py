7bd8ea63fd710fb2b41202d71d81a39f01734d29
Arthur Bernardes <105875484+ArthurrBernardes@users.noreply.github.com>
2022-12-08 11:37:14 -0700

    Update Desafio3.py
    Adding a new way of printing with format. Using f and {} instead of writing format at the and with the variables

--- Project/Desafios/Desafio3.py
+++ Project/Desafios/Desafio3.py
@@ -14,3 +14,6 @@ soma = n1+n2
 # print('A soma entre ', n1, ' e ', n2, ' vale ', soma)
 print('A soma entre {} e {} vale {}'.format(n1, n2, soma))
 
+#ou pode simplesmente colocar um print assim
+print(f'A soma entre {n1} e {n2} vale {soma}')
+
